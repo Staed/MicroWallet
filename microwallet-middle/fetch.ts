427f092a7f89a653ec81acf772f2dfda8d35003c
Kelsey <ky.staedler@gmail.com>
2023-04-11 18:56:16 -0400

    Test insertions into MongoDB collections

--- microwallet-middle/fetch.ts
+++ microwallet-middle/fetch.ts
@@ -1,18 +1,55 @@
-import { PrismaClient } from ".prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma: PrismaClient = new PrismaClient();
 
 async function fetch() {
     await prisma.$connect()
-    await prisma.user.create({
-        data: {
+    // await prisma.user.create({
+    //     data: {
+    //         name: 'Staed'
+    //     }
+    // });
+    // await prisma.coin.createMany({
+    //     data: [
+    //         { name: 'Bitcoin' },
+    //         { name: 'Etherum'}
+    //     ]
+    // })
+
+    const user = await prisma.user.findFirst({
+        where: {
             name: 'Staed'
         }
     });
+    console.log(user);
 
-    const users = await prisma.user.findMany();
+    const coin = await prisma.coin.findFirst({
+        where: {
+            name: 'Bitcoin'
+        }
+    });
+    console.log(coin);
 
-    console.log(users);
+    if (user === undefined || user === null || coin === undefined || coin === null)
+        return;
+    else {
+        // await prisma.transaction.create({
+        //     data: {
+        //         owner: {
+        //             connect: { id: user.id }
+        //         } as any,
+        //         type: {
+        //             connect: { id: coin.id }
+        //         } as any,
+        //         amount: Number(-2.0) as any,
+        //         notes: 'Test Insertion3'
+        //     }
+        // });
+
+        const transactions = await prisma.transaction.findMany();
+
+        console.log(transactions);
+    }
 }
 
 fetch()
